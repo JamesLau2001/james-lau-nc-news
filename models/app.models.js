2fc657e4af414325c4770a07ed697439ef2b4a94
james <james_lau2001@outlook.com>
2024-08-24 18:11:50 +0100

    completed task 6

--- models/app.models.js
+++ models/app.models.js
@@ -46,8 +46,9 @@ if (article_id){
   queryString += ` WHERE article_id = $1`
   queryVal.push(article_id)
 }
+queryString += ` ORDER BY created_at ASC`
 return db.query(queryString, queryVal).then(({rows})=>{
-  
+  console.log(rows)
   return rows
 })
 }
