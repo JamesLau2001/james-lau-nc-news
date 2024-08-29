70d76cceb47db6e1b125fc91e939f3da62e45510
james <james_lau2001@outlook.com>
2024-08-29 11:38:26 +0100

    .

--- listen.js
+++ listen.js
@@ -1,10 +1,10 @@
 const app = require("./app/app.js");
 const { PORT = 9090 } = process.env;
 
-app.listen(PORT, (err) =>{
-    if (err) {
-        console.log(err)
-    } else {
-        console.log(`listening on port ${PORT}`)
-    }
-})
+app.listen(PORT, (err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(`listening on port ${PORT}`);
+  }
+});
