9c75365739984f2fe54e2249dd3953bc739d8c38
james <james_lau2001@outlook.com>
2024-09-16 10:47:00 +0100

    allow corss origin resource sharing

--- app/app.js
+++ app/app.js
@@ -3,8 +3,11 @@ const apiRouter = require("../routers/api.router");
 const articlesRouter = require("../routers/articles.router");
 const commentsRouter = require("../routers/comments.router");
 const usersRouter = require("../routers//users.router");
-
 const app = express();
+const cors = require("cors");
+app.use(cors());
+
+
 app.use(express.json());
 
 app.use("/api", apiRouter);
